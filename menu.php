a3626e59546fff863aacd98e3edaf2b5f0f8d85b
hiagoads <slvhiago2@gmail.com>
2025-11-21 20:55:41 -0300

    adicionado o módulo de menu
    Com esse módulo o usuário poderá navegar entre as páginas do site

---
+++ menu.php
@@ -0,0 +1,18 @@
+<?php 
+
+    include_once ("loginadm.php")
+
+?>
+ 
+ <nav class="menu">
+            <ul>
+                <li><a href="?pg=principal">INÍCIO</a></li>
+                <li><a href="#quemsomos">QUEM SOMOS</a></li>
+                <li><a href="#informacoes">LOCALIZAÇÃO</a></li>
+                <li><a href="?pg=contato">CONTATO</a></li>
+                <li><a href="#" id="loginLink">PAINEL ADM</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+<script src="script.js"></script>
